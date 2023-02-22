c4e54368a1a8f3fe40f8ef40de5b7ab1753c493a
09371116 <mete.capar@ext.kocdigital.com>
2023-02-23 01:31:34 +0300

    changes

--- python/rabbitmq/definitionExport.py
+++ python/rabbitmq/definitionExport.py
@@ -17,7 +17,7 @@ def getDefinition(host, user, password):
 def writeData(definition):
     conn = psycopg2.connect(host=db, database="devops_template", user=user, password=dbPassword, port="5432")
     imlec = conn.cursor()
-    insertQuery = 'INSERT INTO definitions (definitionsRabbit) VALUES ('+definition+');'
+    insertQuery = 'INSERT INTO definitions (definitionsRabbit) VALUES (%s);',(definition)
     imlec.execute(insertQuery)
     conn.commit()
 
