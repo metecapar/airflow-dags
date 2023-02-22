5f37197eee4e36cfc749a2adcfcc8cd0082fc6d9
09371116 <mete.capar@ext.kocdigital.com>
2023-02-22 12:43:39 +0300

    changes

---
+++ dag3.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.configuration import conf
+from airflow.models import Variable
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.operators.bash import BashOperator
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2022, 1, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+def hello_world():
+    print('mete ali emre airflow 101')
+    
+with DAG(
+    dag_id='example_kubernetes_pod', 
+    schedule='@once', 
+    default_args=default_args
+) as dag: KubernetesPodOperator(
+        namespace='dev',
+        image="hello-world",
+        labels={"test": "mete"},
+        name="airflow-test-pod",
+        task_id="task-one",
+        in_cluster=False,  # if set to true, will look in the cluster, if false, looks for file
+        #config_file='/opt/airflow/configfile/config.json', 
+        kubernetes_conn_id='kdtest',
+        is_delete_operator_pod=False,
+        get_logs=True
+    )
