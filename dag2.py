2c455288ed6e049532f7d35ce36e8fdc681211ec
09371116 <mete.capar@ext.kocdigital.com>
2023-02-22 12:26:44 +0300

    changes

--- dag2.py
+++ dag2.py
@@ -7,7 +7,6 @@ from airflow.operators.bash import BashOperator
 
 
 
-matador = Variable.get('matador')
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -22,25 +21,15 @@ with DAG(
     dag_id='example_kubernetes_pod', 
     schedule='@once', 
     default_args=default_args
-) as dag:
-    bash_task_one = BashOperator(
-        task_id="bash_task_one",
-        bash_command='mkdir /opt/airflow/configfile'
-    )
-    bash_task = BashOperator(
-        task_id="bash_task",
-        bash_command='echo "{{ var.value.matador }}" > /opt/airflow/configfile/config.json'
-    )
-    task_one = KubernetesPodOperator(
+) as dag: KubernetesPodOperator(
         namespace='dev',
         image="hello-world",
         labels={"test": "mete"},
         name="airflow-test-pod",
         task_id="task-one",
         in_cluster=False,  # if set to true, will look in the cluster, if false, looks for file
-        cluster_context="",  # is ignored when in_cluster is set to True
-        config_file='/opt/airflow/configfile/config.json', 
+        #config_file='/opt/airflow/configfile/config.json', 
+        kubernetes_conn_id='matador',
         is_delete_operator_pod=True,
-        get_logs=True,
+        get_logs=True
     )
-    bash_task_one>>bash_task >> task_one
