085bcf35584d41c46aa3d6d9b21256a69c55b6d0
09371116 <mete.capar@ext.kocdigital.com>
2023-03-07 01:27:49 +0300

    WIP: 7f9da34 changes

---
+++ dag6.py
@@ -0,0 +1,40 @@
+from airflow import DAG
+from airflow.providers.docker.operators.docker import DockerOperator
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2022, 1, 1),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'kubernetes_docker_example',
+    default_args=default_args,
+    schedule_interval=timedelta(days=1),
+)
+
+with dag:
+    kubernetes_task = KubernetesPodOperator(
+        task_id='kubernetes_task',
+        name='kubernetes-task',
+        namespace='default',
+        image='python:3.8',
+        cmds=['python', '-c'],
+        arguments=['print("Hello, Kubernetes!")'],
+        is_delete_operator_pod=True,
+        get_logs=True,
+    )
+
+    docker_task = DockerOperator(
+        task_id='docker_task',
+        image='docker/whalesay:latest',
+        command='echo "Hello, Docker!"',
+        api_version='auto',
+        auto_remove=True,
+    )
+
+    kubernetes_task >> docker_task
