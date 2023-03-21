8c163e58ffba5b5ec8f50d0a0fa1f9332e307666
09371116 <mete.capar@ext.kocdigital.com>
2023-03-21 23:55:40 +0300

    commit

---
+++ dag7.py
@@ -0,0 +1,31 @@
+from airflow import DAG
+from airflow.providers.docker.operators.docker import DockerOperator
+from kubernetes.client import models as k8s
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2022, 1, 1),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'kubernetes_queue_check',
+    default_args=default_args,
+    schedule_interval='*/5 * * * *'
+)
+with dag:
+    kubernetes_task = KubernetesPodOperator(
+        task_id='kubernetes_task',
+        name='kubernetes-fo-task',
+        namespace='dev',
+        image="ford-queue-checker",
+        image_pull_secrets=[k8s.V1LocalObjectReference("acr")],
+        kubernetes_conn_id='fo',
+        is_delete_operator_pod=False,
+        get_logs=True,
+    )
+    kubernetes_task
